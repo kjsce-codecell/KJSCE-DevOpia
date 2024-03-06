2b221778aff95657dc67d2289c73b2fe63331347
VinayKanse <vinaykanse9451@gmail.com>
2024-03-07 01:53:46 +0530

    Add background image and emblem to Stats page, other stats assets

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -2,12 +2,31 @@ import './Stats.scss'
 function Stats() {
   return (
     <section className="statsContainer">
+      <div className="background"></div>
       <div className="subheading">
         <div>
+          <img src="./Emblem.svg" className="left" alt="" />
           <h3>Stats</h3>
+          <img src="./Emblem.svg" className="right" alt="" />
         </div>
         <p>Stats</p>
       </div>
+
+      {/* <div className="statsGrid">
+        <div className="sideWrapper">
+          <div className="column">
+            <div className="imageContainer">
+              <img src="./Stats/Image1.png" alt="" />
+            </div>
+          </div>
+          <div className="column"></div>
+        </div>
+        <div className="middle"></div>
+        <div className="sideWrapper">
+          <div className="column"></div>
+          <div className="column"></div>
+        </div>
+      </div> */}
     </section>
   )
 }
