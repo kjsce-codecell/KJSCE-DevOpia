db28722e7cdaab298b7609d5d721e8012be72bb4
VinayKanse <vinaykanse9451@gmail.com>
2024-03-07 00:23:58 +0530

    Setup Stats Section

---
+++ src/pages/Stats/Stats.jsx
@@ -0,0 +1,15 @@
+import './Stats.scss'
+function Stats() {
+  return (
+    <section className="statsContainer">
+      <div className="subheading">
+        <div>
+          <h3>Stats</h3>
+        </div>
+        <p>Stats</p>
+      </div>
+    </section>
+  )
+}
+
+export default Stats
