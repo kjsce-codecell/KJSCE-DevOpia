499755492fb6ce01bb1ed20be62ba961ff41d182
VinayKanse <vinaykanse9451@gmail.com>
2024-03-07 21:24:56 +0530

    Delete unused images and update dev script in package.json, Update Stats section

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -12,21 +12,72 @@ function Stats() {
         <p>Stats</p>
       </div>
 
-      {/* <div className="statsGrid">
+      <div className="statsGrid">
         <div className="sideWrapper">
           <div className="column">
             <div className="imageContainer">
               <img src="./Stats/Image1.png" alt="" />
             </div>
+            <div className="content">
+              <h4 className="highlightNum">24</h4>
+              <p>hack hours</p>
+            </div>
+            <div className="imageContainer">
+              <img src="./Stats/Image2.png" alt="" />
+            </div>
           </div>
-          <div className="column"></div>
+          <div className="column">
+            <div className="imageContainer">
+              <img src="./Stats/Image3.png" alt="" />
+            </div>
+            <div className="content">
+              <h4 className="highlightNum">100+</h4>
+              <p>colleges</p>
+            </div>
+            <div className="imageContainer">
+              <img src="./Stats/Image4.png" alt="" />
+            </div>
+          </div>
         </div>
-        <div className="middle"></div>
+        <div className="middle">
+          <div className="imageContainer">
+            <img src="./Stats/Image5.png" alt="" />
+          </div>
+          <div className="content">
+            <h4 className="highlightNum">2000+</h4>
+            <p>Registrations</p>
+          </div>
+          <div className="imageContainer">
+            <img src="./Stats/Image6.png" alt="" />
+          </div>
+        </div>
         <div className="sideWrapper">
-          <div className="column"></div>
-          <div className="column"></div>
+          <div className="column">
+            <div className="imageContainer">
+              <img src="./Stats/Image7.png" alt="" />
+            </div>
+            <div className="content">
+              <h4 className="highlightNum">150+</h4>
+              <p>Hackers</p>
+            </div>
+            <div className="imageContainer">
+              <img src="./Stats/Image8.png" alt="" />
+            </div>
+          </div>
+          <div className="column">
+            <div className="imageContainer">
+              <img src="./Stats/Image9.png" alt="" />
+            </div>
+            <div className="content">
+              <h4 className="highlightNum">40+</h4>
+              <p>Projects</p>
+            </div>
+            <div className="imageContainer">
+              <img src="./Stats/Image10.png" alt="" />
+            </div>
+          </div>
         </div>
-      </div> */}
+      </div>
     </section>
   )
 }
