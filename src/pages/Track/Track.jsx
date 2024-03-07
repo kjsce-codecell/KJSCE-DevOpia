4a910131609e855f08f5bd4691d7030b3da37877
viraj <vlbhartiya@gmail.com>
2024-03-08 01:53:36 +0530

    animation on tracks page

--- src/pages/Track/Track.jsx
+++ src/pages/Track/Track.jsx
@@ -1,15 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './Track.scss';
 import First from '../../assets/track.png';
+import { motion } from 'framer-motion';
 
 const Track = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        rootMargin: '-100px', // Adjust this value as needed
+      }
+    );
+
+    const trackElement = document.getElementById('track');
+    if (trackElement) {
+      observer.observe(trackElement);
+    }
+
+    return () => {
+      if (trackElement) {
+        observer.unobserve(trackElement);
+      }
+    };
+  }, []);
+
   return (
     <section id='track' className="track">
       <div className="content">
-        <h1 className="title">Track</h1>
-        <p className="subtitle">Track</p>
+        <h1 className="title"
+
+        >
+          Track
+        </h1>
+        <motion.p className="subtitle"
+          animate={{
+            opacity: isVisible ? 1 : 0,
+            transition: { duration: 1.5, ease: 'easeIn' }
+          }}
+        >
+          Track
+        </motion.p>
       </div>
       <div className="tracks">
-        <div className="prize">
+        <motion.div className="prize"
+          animate={{
+            opacity: isVisible ? 1 : 0,
+            y: isVisible ? 0 : 50,
+            transition: { duration: 1.5, ease: 'easeIn' }
+          }}
+        >
           <div className="desc">
             <h2>inr 10,000 for the best hack built on Ethereum<br /><br />
               Continuity grant opportunity up to $5,000 for outstanding projects<br /><br />
@@ -17,8 +63,14 @@ const Track = () => {
             <p> SPONSOR WINNER</p>
           </div>
           <img src={First} alt="" />
-        </div>
-        <div className="prize">
+        </motion.div>
+        <motion.div className="prize"
+          animate={{
+            opacity: isVisible ? 1 : 0,
+            y: isVisible ? 0 : 50,
+            transition: { duration: 1.5, ease: 'easeIn' }
+          }}
+        >
           <div className="desc">
             <h2>inr 10,000 for the best hack built on Ethereum<br /><br />
               Continuity grant opportunity up to $5,000 for outstanding projects<br /><br />
@@ -26,8 +78,14 @@ const Track = () => {
             <p> SPONSOR WINNER</p>
           </div>
           <img src={First} alt="" />
-        </div>
-        <div className="prize">
+        </motion.div>
+        <motion.div className="prize"
+          animate={{
+            opacity: isVisible ? 1 : 0,
+            y: isVisible ? 0 : 50,
+            transition: { duration: 1.5, ease: 'easeIn' }
+          }}
+        >
           <div className="desc">
             <h2>inr 10,000 for the best hack built on Ethereum<br /><br />
               Continuity grant opportunity up to $5,000 for outstanding projects<br /><br />
@@ -35,7 +93,7 @@ const Track = () => {
             <p> SPONSOR WINNER</p>
           </div>
           <img src={First} alt="" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
