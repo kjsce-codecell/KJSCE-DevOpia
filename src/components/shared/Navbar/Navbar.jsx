1141bfc78efab422d3aa270fb61fcd57f9f2563c
VinayKanse <vinaykanse9451@gmail.com>
2024-03-07 23:07:41 +0530

    Refactor Navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,83 +1,113 @@
-import { useEffect, useState } from 'react';
-import './Navbar.scss';
-import Pronged from '../../../assets/pronged.png';
-import CodeCell from '../../../assets/CodeCell.png';
-import CSI from '../../../assets/CSI.png';
-import SVU from '../../../assets/svu.png';
-import Somaiya from '../../../assets/somaiya.png';
+import { useEffect, useState } from 'react'
+import './Navbar.scss'
+import Pronged from '../../../assets/pronged.png'
+import CodeCell from '../../../assets/CodeCell.png'
+import CSI from '../../../assets/CSI.png'
+import SVU from '../../../assets/svu.png'
+import Somaiya from '../../../assets/somaiya.png'
+
+const menuList = [
+  {
+    text: 'Home',
+    link: '#home',
+  },
+  {
+    text: 'About',
+    link: '#about',
+  },
+  {
+    text: 'Stats',
+    link: '#stats',
+  },
+  {
+    text: 'Prizes',
+    link: '#prize',
+  },
+  {
+    text: 'Sponsors',
+    link: '#sponsors',
+  },
+  {
+    text: 'Faqs',
+    link: '#faqs',
+  },
+]
 
 function Navbar() {
-  const [isOpen, setisOpen] = useState(false);
-  const [binaryIndex, setBinaryIndex] = useState(0);
+  const [isOpen, setisOpen] = useState(false)
+  const [binaryIndex, setBinaryIndex] = useState(0)
   const binaryArray = [
     '01001001 00100111 01101101 00100000 01000010 01100001 01110100 01101101 01100001 01101110 01001001 00100111 01101101 00100000 01000010 01100001 01110100 01101101 01100001 01101110 01001001 00100111 01101101 00100000 01000010 01100001 01110100 01101101 01100001 01101110',
     '01010100 01101000 01100101 00100000 01001010 01110101 01110011 01110100 01101001 01100011 01100101 00100000 01001100 01100101 01100001 01100111 01110101 01100101 00100000 01001110 01100101 01100101 01100100 01110011 00100000 01011001 01101111 01110101 00100001',
-  ];
-  const [activeSection, setActiveSection] = useState('Home');
+  ]
+  const [activeSection, setActiveSection] = useState('Home')
 
   function handleClick() {
-    setisOpen(!isOpen);
+    setisOpen(!isOpen)
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setBinaryIndex((prevIndex) => (prevIndex + 1) % binaryArray.length);
-    }, 500);
+      setBinaryIndex((prevIndex) => (prevIndex + 1) % binaryArray.length)
+    }, 500)
 
-    return () => clearInterval(interval);
-  }, [binaryArray.length]);
+    return () => clearInterval(interval)
+  }, [binaryArray.length])
 
   useEffect(() => {
     function handleScroll() {
-      const sections = document.querySelectorAll('section');
+      const sections = document.querySelectorAll('section')
       sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.clientHeight
         if (
           window.scrollY + 125 >= sectionTop &&
           window.scrollY < sectionTop + sectionHeight
         ) {
-          setActiveSection(section.id);
+          setActiveSection(section.id)
         }
-      });
+      })
     }
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-    });
-  };
+    })
+  }
 
   return (
     <>
       <nav className="navbar">
         <div
-          className={`icon nav-icon-1 ${isOpen ? 'open' : ''}`}
+          className={`nav-icon ${isOpen ? 'open' : ''}`}
           onClick={handleClick}
         >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <img
+            src={isOpen ? './BurgerClose.svg' : './BurgerSquare.svg'}
+            alt=""
+          />
         </div>
         <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-
-          <ul className='links' >
-            <li className="nav-link"><a onClick={handleClick} href="#home">Home</a></li>
-            <li className="nav-link"><a onClick={handleClick} href="#about">About</a></li>
-            <li className="nav-link"><a onClick={handleClick} href="#stats">Stats</a></li>
-            <li className="nav-link"><a onClick={handleClick} href="#prize">Prizes</a></li>
-            <li className="nav-link"><a onClick={handleClick} href="#sponsors">Sponsors</a></li>
-            <li className="nav-link"><a onClick={handleClick} href="#faqs">Faq</a></li>
+          <ul className="links">
+            {menuList.map((menu) => (
+              <li className="nav-link" key={menu.text}>
+                <a
+                  onClick={handleClick}
+                  href={menu.link}
+                  className={
+                    menu.text.toLowerCase() === activeSection.toLowerCase()
+                      ? 'active'
+                      : ''
+                  }
+                >
+                  {menu.text}
+                </a>
+              </li>
+            ))}
           </ul>
           <div className="logos">
             <img src={CodeCell} alt="" />
@@ -88,22 +118,16 @@ function Navbar() {
         </div>
         <div className="active">
           <div className="active-link-name">{activeSection}</div>
-          <div className="border">
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-          </div>
         </div>
 
         <div className="logo" onClick={scrollToTop}>
           <img src={Pronged} alt="" />
         </div>
 
-
         <p className="binary">{binaryArray[binaryIndex]}</p>
       </nav>
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/pages/Prizes/Prizes.jsx
+++ src/pages/Prizes/Prizes.jsx
@@ -1,10 +1,10 @@
-import './Prizes.scss';
-import First from '../../assets/first.png';
-import Second from '../../assets/second.png';
-import Third from '../../assets/third.png';
+import './Prizes.scss'
+import First from '../../assets/first.png'
+import Second from '../../assets/second.png'
+import Third from '../../assets/third.png'
 const Prizes = () => {
   return (
-    <section className="prize" id="prize">
+    <section className="prize" id="prizes">
       <div className="prizes">
         <div className="background"></div>
         <div className="content">
@@ -12,49 +12,70 @@ const Prizes = () => {
           <p className="subtitle">Prizes</p>
           <div className="prize">
             <div className="grid-container">
-              <div className="left-container" style={{
-                background: `url(${First})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-              }}>
+              <div
+                className="left-container"
+                style={{
+                  background: `url(${First})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                }}
+              >
                 <div className="container-text">
                   <div className="sec">
                     <h2>INR 30,000</h2>
                     <h3>CASH PRIZE</h3>
-                    <h4>Exciting Goodies worth<br /> inr 2l & More!</h4>
+                    <h4>
+                      Exciting Goodies worth
+                      <br /> inr 2l & More!
+                    </h4>
                   </div>
                   <div className="spacer"></div>
                   <p>WINNER</p>
                 </div>
               </div>
               <div className="right-container">
-                <div style={{
-                  background: `url(${Second})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                }} className="right-item"><div className="container-text"><div className="sec">
-                  <div className='row'>
-                    <h2>INR 30,000</h2>
-                    <h3>CASH PRIZE</h3>
-                  </div>
+                <div
+                  style={{
+                    background: `url(${Second})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                  className="right-item"
+                >
+                  <div className="container-text">
+                    <div className="sec">
+                      <div className="row">
+                        <h2>INR 30,000</h2>
+                        <h3>CASH PRIZE</h3>
+                      </div>
 
-                  <h4>Exciting Goodies worth<br /> inr 2l & More!</h4>
-                  <div className="spacer"></div>
-                </div>
+                      <h4>
+                        Exciting Goodies worth
+                        <br /> inr 2l & More!
+                      </h4>
+                      <div className="spacer"></div>
+                    </div>
                     <p>RUNNER UP</p>
                   </div>
                   <img src={Second} alt="" />
                 </div>
-                <div className="right-item"><div className="container-text"><div className="sec">
-                  <div className='row'>
-                    <h2>INR 30,000</h2>
-                    <h3>CASH PRIZE</h3>
-                  </div>
+                <div className="right-item">
+                  <div className="container-text">
+                    <div className="sec">
+                      <div className="row">
+                        <h2>INR 30,000</h2>
+                        <h3>CASH PRIZE</h3>
+                      </div>
 
-                  <h4>Exciting Goodies worth<br /> inr 2l & More!</h4>
-                  <div className="spacer"></div>
-                </div>
-                  <p>2ND RUNNER UP</p></div><img src={Third} alt="" />
+                      <h4>
+                        Exciting Goodies worth
+                        <br /> inr 2l & More!
+                      </h4>
+                      <div className="spacer"></div>
+                    </div>
+                    <p>2ND RUNNER UP</p>
+                  </div>
+                  <img src={Third} alt="" />
                 </div>
               </div>
             </div>
@@ -90,7 +111,7 @@ const Prizes = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Prizes;
+export default Prizes
