0cbfaedaf270928840c44782eb8d70a3a3f6cff5
viraj <vlbhartiya@gmail.com>
2024-03-29 21:27:11 +0530

    added tracks section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Prizes from './pages/Prizes/Prizes'
-// import Track from './pages/Track/Track'
+import Track from './pages/Track/Track'
 import FAQ from './pages/FAQ/FAQ'
 import Navbar from './components/shared/Navbar/Navbar'
 import Footer from './components/shared/Footer/Footer'
@@ -43,7 +43,7 @@ function App() {
       <About />
       <Stats />
       <Prizes />
-      {/* <Track /> */}
+      <Track />
       <Sponsors />
       <FAQ />
       <Footer />
