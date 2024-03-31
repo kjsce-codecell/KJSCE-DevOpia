bce6567748edb50809ac76e00b8308a9618c76ba
Aditya Belgaonkar <adityabelgaonkar.30@gmail.com>
2024-03-31 15:32:42 +0530

    Fix: Animation on track prizes.

--- src/pages/Track/Track.jsx
+++ src/pages/Track/Track.jsx
@@ -13,46 +13,52 @@ const Track = () => {
         </div>
       </div>
       <div className="tracks">
-        <div data-aos="fade-up" data-aos-duration="1000" className="prize">
-          <div className="desc">
-            <h2>
-              $200 - Best hack built on Polygon
-              <br />
-              <br />
-              Read about the bounty details and find code templates for Polygon
-              here
-            </h2>
-            <p> Polygon track</p>
+        <div data-aos="fade-up" data-aos-duration="1000">
+        <div className='prize'>
+            <div className="desc">
+              <h2>
+                $200 - Best hack built on Polygon
+                <br />
+                <br />
+                Read about the bounty details and find code templates for Polygon
+                here
+              </h2>
+              <p> Polygon track</p>
+            </div>
+            <img src={First} alt="" />
           </div>
-          <img src={First} alt="" />
         </div>
-        <div data-aos="fade-up" data-aos-duration="1000" className="prize">
-          <div className="desc">
-            <h2>
-              $100 - Best hack built on Ethereum
-              <br />
-              <br />
-              Select this track if you're building on Ethereum to be eligible
-              for the prizes!
-            </h2>
-            <p>ETHIndia track</p>
+        <div data-aos="fade-up" data-aos-duration="1000">
+          <div className='prize'>
+            <div className="desc">
+              <h2>
+                $100 - Best hack built on Ethereum
+                <br />
+                <br />
+                Select this track if you're building on Ethereum to be eligible
+                for the prizes!
+              </h2>
+              <p>ETHIndia track</p>
+            </div>
+            <img src={First} alt="" />
           </div>
-          <img src={First} alt="" />
         </div>
-        <div data-aos="fade-up" data-aos-duration="1000" className="prize">
-          <div className="desc">
-            <h2>
-              inr 10,000 for the best hack built on Ethereum
-              <br />
-              <br />
-              Continuity grant opportunity up to $5,000 for outstanding projects
-              <br />
-              <br />
-              inr 10,000 for the best project made using Arcana
-            </h2>
-            <p> SPONSOR WINNER</p>
+        <div data-aos="fade-up" data-aos-duration="1000">
+        <div className='prize'>
+            <div className="desc">
+              <h2>
+                inr 10,000 for the best hack built on Ethereum
+                <br />
+                <br />
+                Continuity grant opportunity up to $5,000 for outstanding projects
+                <br />
+                <br />
+                inr 10,000 for the best project made using Arcana
+              </h2>
+              <p> SPONSOR WINNER</p>
+            </div>
+            <img src={First} alt="" />
           </div>
-          <img src={First} alt="" />
         </div>
       </div>
     </section>
